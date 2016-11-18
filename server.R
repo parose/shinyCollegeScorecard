de093325200e354c6d06783eb8988bf87859e246
klmork <kirstenmork@hotmail.com>
2016-11-17 17:52:25 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,8 +1,11 @@
 library(shiny)
+library(datasets)
+library(plotly)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  score <- read.csv("data/Most-Recent-Cohorts-All-Data-Elements.csv")
   # Expression that generates a histogram. The expression is
   # wrapped in a call to renderPlot to indicate that:
   #
@@ -10,11 +13,30 @@ shinyServer(function(input, output) {
   #     when inputs change
   #  2) Its output type is a plot
   
-  output$distPlot <- renderPlot({
+  output$distPlot <- renderPlotly({
     x    <- faithful[, 2]  # Old Faithful Geyser data
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    plot_ly(x = ~rnorm(50), type = "histogram")#(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+  # Return the requested dataset
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = score[0:5],
+           "pressure" = score[6:10],
+           "cars" = score[11:12])
+  })
+  
+  # Generate a summary of the dataset
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  # Show the first "n" observations
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
   })
 })
